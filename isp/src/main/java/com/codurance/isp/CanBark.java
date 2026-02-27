e0e66871c99d0b1b382395fb12d2821d52a80b9d
Parker Downing <parkeraydowning@gmail.com>
2026-02-27 15:28:56 -0700

    Refactors to use isp

---
+++ isp/src/main/java/com/codurance/isp/CanBark.java
@@ -0,0 +1,5 @@
+package com.codurance.isp;
+
+public interface CanBark {
+  void bark();
+}
--- isp/src/main/java/com/codurance/isp/Dog.java
+++ isp/src/main/java/com/codurance/isp/Dog.java
@@ -1,10 +1,6 @@
 package com.codurance.isp;
 
-public class Dog implements Animal {
-    @Override
-    public void fly() {
-
-    }
+public class Dog implements CanRun, CanBark {
 
     @Override
     public void run() {
